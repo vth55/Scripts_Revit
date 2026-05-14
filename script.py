f8c9851137e95665f09f2ae2058bf4dafde37636
Vitor Hugo <vitorhugo22.igrejas@gmail.com>
2026-05-14 15:38:30 +0100

    Clean repository root for distribution

--- script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Filter Manager entrypoint."""
-__title__ = "Filters"
-__author__ = "Vitor Hugo"
-
-import main_window_logic
-reload(main_window_logic)
-main_window_logic.MainWindow().ShowDialog()
